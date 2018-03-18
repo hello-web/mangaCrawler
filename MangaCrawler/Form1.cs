327551f4990d69a3e7b6ec18f38c01c9dd9b6f9b
nothing628 <yogyphang@gmail.com>
2018-03-18 11:21:37 +0700

    basic download string

--- MangaCrawler/Form1.cs
+++ MangaCrawler/Form1.cs
@@ -4,6 +4,8 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,43 @@ namespace MangaCrawler
 {
     public partial class Form1 : Form
     {
+        private delegate void Change();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                string a = await Download(textBox1.Text);
+                Change b = delegate ()
+                {
+                    textBox2.Text = a;
+                };
+
+                Invoke(b);
+            });
+        }
+
+        private async Task<string> Download(string address)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Uri requestUri = new Uri(address);
+                HttpRequestMessage httpRequest = new HttpRequestMessage(HttpMethod.Get, requestUri);
+                HttpResponseMessage httpResponse = await client.SendAsync(httpRequest);
+                
+                if (httpResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    HttpContent content = httpResponse.Content;
+                    return await content.ReadAsStringAsync();
+                }
+
+                return null;
+            }
         }
     }
 }
