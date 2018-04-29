8f65a9d4aa97c1521ca74bfbbc9c57202a2fbf23
nothing628 <yogyphang@gmail.com>
2018-04-29 21:08:33 +0700

    Add table class

---
+++ MangaCrawler/Crawler/Database/Bookmark.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaCrawler.Crawler.Database
+{
+    class Bookmark
+    {
+        public long Id { get; set; }
+        public long IdManga { get; set; }
+        public byte Flag { get; set; }
+    }
+
+    enum BookmarkFlag
+    {
+        PlanToRead,
+        StillRead,
+        FinishRead,
+        Favorite,
+    }
+}
---
+++ MangaCrawler/Crawler/Database/Download.cs
@@ -0,0 +1,25 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MangaCrawler.Crawler.Database
+{
+    [Table("download")]
+    class Download
+    {
+        public long Id { get; set; }
+        public long IdChapter { get; set; }
+        public bool IsDownload { get; set; }
+    }
+
+    enum DownloadStatus
+    {
+        Stopped,
+        Paused,
+        Downloading,
+        Finished,
+    }
+}
